86208af69a4ed6217bd913f325c390427a94bc40
febkosq8 <febkosq8@yahoo.com>
2026-05-09 22:43:07 +0100

    fix: update API base URL for production environment

--- src/handlers/APIHandler.ts
+++ src/handlers/APIHandler.ts
@@ -1,6 +1,8 @@
 import axios from "axios";
 axios.defaults.baseURL =
-	process.env.NODE_ENV === "production" ? "https://serve.febkosq8.me/api/v1" : "http://localhost:3030/api/v1";
+	process.env.NODE_ENV === "production"
+		? "https://serve.febkosq8.me/publicapi/v1"
+		: "http://localhost:3030/publicapi/v1";
 class APIHandler {
 	static async getSmolURL(shortName: string) {
 		const response = await axios.get(`/smolURL?shortName=${shortName}`);
